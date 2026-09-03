f224772578cfb645563b098ca183e5c1a845fe96
MNMF <50932052+MNMF@users.noreply.github.com>
2026-09-03 11:07:43 -0400

    Add files via upload

---
+++ lib/areas.ts
@@ -0,0 +1,11 @@
+export type Area = {
+  slug: string;
+  nombre: string;
+};
+
+export const areas: Area[] = [
+  { slug: "fonoaudiologia", nombre: "Atención Fonoaudiológica" },
+  { slug: "terapia-ocupacional", nombre: "Atención Terapia Ocupacional" },
+  { slug: "psicologia", nombre: "Atención Psicológica" },
+  { slug: "psicopedagogia", nombre: "Atención Psicopedagógica" },
+];
