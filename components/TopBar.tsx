86eaffea78983cf1487cc9f39af8b23df2f1a3c0
MNMF <50932052+MNMF@users.noreply.github.com>
2026-09-04 10:02:14 -0400

    Update TopBar.tsx

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -40,7 +40,7 @@ export default function TopBar() {
             <path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z" />
             <circle cx="12" cy="10" r="3" />
           </svg>
-          <span>Edificio Plaza Talca, Piso 12</span>
+          <span>Edificio Plaza Talca, Piso 12 - Oficina 1210 </span>
         </a>
       </div>
     </div>
